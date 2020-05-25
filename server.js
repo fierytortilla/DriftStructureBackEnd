56b03cd6cfce32a12372702ce641573d4ba1377a
fierytortilla <dschlessinger@live.com>
2020-05-25 12:08:19 +0100

    built a functional backend to host the songs on

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express'); 
+const app = express(); 
+const path = require('path');
+const cors = require("cors");
+
+app.use(express.static(path.join(__dirname, 'media')))
+app.use(cors());
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+// from front end make request to 'http://localhost:3000/heat.mp3'
+app.get('/media/:soundFile', (req, res) => {
+  res.sendFile( __dirname + `/media/${req.params.soundFile}` );
+}) 
+
+app.listen(3000, function () { // NEW
+  console.log('App running on port 3000');
+});
