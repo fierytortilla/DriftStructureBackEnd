2b6b624df965a0619bd311f929859c10b615a3a3
fierytortilla <dschlessinger@live.com>
2020-05-26 19:49:16 +0100

    changed some songs, started making minor changes to integrate the songs into mongoDB

--- server.js
+++ server.js
@@ -10,6 +10,17 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
+app.sendSongs('/media', (req, res) => {
+  // Get mongoDB collection of songs
+  /**
+   * {
+   *   name: String,
+   *   url: String
+   * }
+   */
+  
+}) 
+
 // from front end make request to 'http://localhost:3000/heat.mp3'
 app.get('/media/:soundFile', (req, res) => {
   res.sendFile( __dirname + `/media/${req.params.soundFile}` );
