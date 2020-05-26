6b122fcfe0386549cf337ae04cb3e4c78f9114a3
fierytortilla <dschlessinger@live.com>
2020-05-27 00:15:38 +0100

    expanding backend

---
+++ db/seeds.js
@@ -0,0 +1,32 @@
+// use driftStructure;
+// db.dropDatabase();
+
+// Server side exports and imports
+// const import = require()
+
+// exports.varName = whatever
+// module.exports = the whole damn thing
+
+module.exports = [
+    {
+        "title": "Sounded",
+        "artist":"Trenches of Hadal",
+        "url":"localhost:3000/media/sounded.mp3"
+    },
+    {
+        "title": "Dheirich Mi Moch Madainn Cheothar",
+        "artist":"Julia Fowlis",
+        "url":"localhost:3000/media/dheirichMiMochMadainnCheothar.mp3"   
+    },
+    {
+        "title": "Beaming Husband",
+        "artist":"Geotic",
+        "url":"localhost:3000/media/beamingHusband.mp3"
+    },
+    {
+        "title": "Bit About Yourself",
+        "artist":"Johnny Greenwood",
+        "url":"localhost:3000/media/bitAboutYourself.mp3"
+    }
+];
+
--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express(); 
 const path = require('path');
 const cors = require("cors");
+// const MongoClient = require('mongodb').MongoClient; 
+const songs = require('./db/seeds.js')
 
 app.use(express.static(path.join(__dirname, 'media')))
 app.use(cors());
@@ -10,18 +12,21 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-// app.sendSongs('/media', (req, res) => {
-//   // Get mongoDB collection of songs
-//   /**
-//    * {
-//    *   name: String,
-//    *   url: String
-//    * }
-//    */
-  
-// }) 
+app.get('/media', (req, res) => {
+  // Get mongoDB collection of songs
+  /**
+   * {
+   *   name: String,
+   *   url: String
+   * }
+  //  */
+  // for(song in songs){
+  res.json(songs)
+    // res.sendFile(__dirname+`/media/${req.params.soundFile}`);
+  // }
+}) 
 
-// from front end make request to 'http://localhost:3000/heat.mp3'
+// from front end make request to 'http://localhost:3000/songName.mp3'
 app.get('/media/:soundFile', (req, res) => {
   res.sendFile( __dirname + `/media/${req.params.soundFile}` );
 }) 
